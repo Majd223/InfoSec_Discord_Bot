abd45e73c85a31d72c410f3056a4c3c88008881d
Majd <majd_safi_1@hotmail.com>
2021-05-30 23:55:19 -0700

    Added Welcoming Feature.

--- main.js
+++ main.js
@@ -5,8 +5,12 @@ const client = new Discord.Client();
 //set the prefix command for the bot.
 const prefix = '*';
 
+//get the bot token/channel IDs from ServerIDs.json
+var IDs = require("./ServerIDs.json");
+
 //Search the folder "commands" that contains files for the bot commands
 const fs = require('fs');
+const Welcome = require('./commands/Welcome');
 
 client.commands = new Discord.Collection();
 
@@ -22,25 +26,36 @@ function greenMessage(text) {
     console.log(chalk.green(text) + "\n");
 }
 
-//Prepare the bot and getit online.
-client.once('ready', () => {
+//Output a message when the bot is ready and online.
+client.on('ready', () => {
     greenMessage('Infosec bot is online!');
 })
 
 //command test.
 client.on('message', message => {
+    
     //if the message doesnt start with the prefix then ignore it.
     if (!message.content.startsWith(prefix) || message.author.bot) return; 
 
     //do a slice to be able to do multiple command, for example *check calender
-    const args = message.content.slice(prefix.length).split(" ");
+    const args = message.content.slice(prefix.length).split(/ +/);
     const command = args.shift().toLowerCase();
 
-    //if the command is ... , then reply ...
+    //if the command is ... , then execute ...
     if(command === 'stonks') {
         client.commands.get('stonks').execute(message, args);
-    }
-})
+    } 
+});
+
+//Welcome new people.
+client.on('guildMemberAdd', member =>{
+    //create a const of the channel you want to welcome people in.
+    const channel = member.guild.channels.cache.get(IDs.WelcomeChannel);
+    if (!channel) return;
+
+    //Welcome Message.
+    channel.send(`Welcome to the server, ${member}`);
+});
 
 //This line should stay the last line, it auth the bot with the discord application.
-client.login(''); 
+client.login(IDs.token); 
